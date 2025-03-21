ea970619f1efdfcf7e2f51f7116af4e3d952dd0e
Willians Martins <professorwillians@gmail.com>
2025-03-21 04:53:03 +0000

    add

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -22,6 +22,12 @@ function App() {
       image:
         'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
     },
+    {
+      name: 'Willians Martins',
+      email: 'willians.martins@example.com',
+      image:
+        'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
   ]
 
   return (
